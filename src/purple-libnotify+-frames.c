428dde74ad58ebebb916de528dc799f6d8c9b158
Quentin Glidic <sardemff7+git@sardemff7.net>
2012-03-11 13:11:05 +0100

    Kill remaining pidgin-libnotify+ references

--- src/purple-libnotify+-frames.c
+++ src/purple-libnotify+-frames.c
@@ -1,21 +1,21 @@
 /*
- * Pidgin-Libnotify+ - Provide libnotify interface to Pidgin
+ * purple-libnotify+ - Provide libnotify interface to Pidgin and Finch
  * Copyright © 2010-2011 Quentin "Sardem FF7" Glidic
  *
- * This file is part of Pidgin-Libnotify+.
+ * This file is part of purple-libnotify+.
  *
- * Pidgin-Libnotify+ is free software: you can redistribute it and/or modify
+ * purple-libnotify+ is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Pidgin-Libnotify+ is distributed in the hope that it will be useful,
+ * purple-libnotify+ is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Pidgin-Libnotify+.  If not, see <http://www.gnu.org/licenses/>.
+ * along with purple-libnotify+.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "purple-libnotify+-common.h"
