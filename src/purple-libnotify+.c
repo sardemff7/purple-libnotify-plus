a45caf8bdad11ebc686b7a8a87a588699c4d0731
xvan <jzuloaga@jzuloaga-dsk.edrans.net>
2014-05-13 10:58:12 -0300

    Do not send a notification twice
    Bug introduced in d512d270b8fe5ee2b4943dec12db2e1e82105843

--- src/purple-libnotify+.c
+++ src/purple-libnotify+.c
@@ -84,8 +84,6 @@ _purple_notify_plus_im_message(PurpleAccount *account, const gchar *sender, cons
 	body = g_strdup_printf(_("“%s”"), tmp);
 	g_free(tmp);
 
-	notify_plus_send_buddy_notification(buddy, "%s", body);
-
 	if ( buddy != NULL )
 		notify_plus_send_buddy_notification(buddy, "%s", body);
 	else
@@ -123,8 +121,6 @@ _purple_notify_plus_chat_message(PurpleAccount *account, const gchar *sender, co
 	tmp = purple_markup_strip_html(message);
 	body = g_strdup_printf(_("“%s”"), tmp);
 	g_free(tmp);
-
-	notify_plus_send_buddy_notification(buddy, "%s", body);
 
 	if ( buddy != NULL )
 		notify_plus_send_buddy_notification(buddy, "%s", body);
